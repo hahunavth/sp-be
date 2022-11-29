438ad7ff94bda65847ea06e0087ca9323c11b3cd
Hahunavth <vuthanhha.2001@gmail.com>
2022-11-29 15:29:49 +0700

    feat(Refactor app): :sparkles:  ## Change log:- Change: / using jade- BaseURL:
    /api/v1- Enable CORS- Add swagger dock- Run script:   - Development:      npm run dev  - Produ
    
    ction:      npm run start

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-$ npm install
+### Setup:
 
+```bash
+npm install
+```
 
+## Change log:
+
+- Change: / using jade
+- BaseURL: /api/v1
+- Enable CORS
+- Add swagger dock
+- Run script: 
+  - Development:
+      npm run dev
+  - Production:
+      npm run start
--- app.js
+++ app.js
@@ -1,30 +1,74 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var router = express.Router();
-const connectDb = require('./db.js');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-var importHistory = require('./routes/importHistory');
+var router = express.Router();
+const connectDb = require("./db.js");
+
+var importHistory = require("./routes/importHistory");
 
 var app = express();
 connectDb;
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 let port = process.env.PORT || 8080;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/importProduct', importHistory);
+// NOTE: add new routes here!
+app.use("/api/v1/importProduct", importHistory);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "SP_17",
+    messages: [
+      {
+        msg: "Hello there!",
+      },
+      {
+        msg: "Base url: ",
+        link: "http://localhost:" + port + "/api/v1",
+      },
+      {
+        msg: "Nhap hang: ",
+        link: "http://localhost:" + port + "/api/v1/importProduct",
+      },
+      {
+        msg: "Xuat hang: ",
+        link: "http://localhost:" + port + "/api/v1/exportProduct",
+      },
+      {
+        msg: "Nha cung cap: ",
+        link: "http://localhost:" + port + "/api/v1/supplier",
+      },
+      {
+        msg: "Swagger: ",
+        link: "https://app.swaggerhub.com/apis/VUTHANHHA2001/spdoc/1.0.11#/importProduct/get_importProduct",
+      },
+    ],
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,17 +79,17 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 app.listen(port, function () {
-  const host = 'localhost';
+  const host = "localhost";
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
 });
 
 module.exports = app;
