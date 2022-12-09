91ab71cd951838d38b7a82f62a47ac108a5cca38
Hahunavth <vuthanhha.2001@gmail.com>
2022-12-09 15:54:40 +0700

    feat(Add crud service): :sparkles:

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,13 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import { CreateUserDto } from '@dtos/users.dto';
 import { User } from '@interfaces/users.interface';
 import userService from '@services/users.service';
+import CRUDService from '@/services/crud.service';
 
 class UsersController {
   public userService = new userService();
 
   public getUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const findAllUsersData: User[] = await this.userService.findAllUser();
+      const findAllUsersData: User[] = await this.userService.findAll();
 
       res.status(200).json({ data: findAllUsersData, message: 'findAll' });
     } catch (error) {
@@ -19,7 +20,7 @@ class UsersController {
   public getUserById = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId = Number(req.params.id);
-      const findOneUserData: User = await this.userService.findUserById(userId);
+      const findOneUserData: User = await this.userService.findById(userId);
 
       res.status(200).json({ data: findOneUserData, message: 'findOne' });
     } catch (error) {
@@ -30,7 +31,7 @@ class UsersController {
   public createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userData: CreateUserDto = req.body;
-      const createUserData: User = await this.userService.createUser(userData);
+      const createUserData: User = await this.userService.create(userData);
 
       res.status(201).json({ data: createUserData, message: 'created' });
     } catch (error) {
@@ -42,7 +43,7 @@ class UsersController {
     try {
       const userId = Number(req.params.id);
       const userData: CreateUserDto = req.body;
-      const updateUserData: User = await this.userService.updateUser(userId, userData);
+      const updateUserData: User = await this.userService.update(userId, userData);
 
       res.status(200).json({ data: updateUserData, message: 'updated' });
     } catch (error) {
@@ -53,7 +54,7 @@ class UsersController {
   public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId = Number(req.params.id);
-      const deleteUserData: User = await this.userService.deleteUser(userId);
+      const deleteUserData: User = await this.userService.delete(userId);
 
       res.status(200).json({ data: deleteUserData, message: 'deleted' });
     } catch (error) {
---
+++ src/services/crud.service.ts
@@ -0,0 +1,77 @@
+import { hash } from 'bcrypt';
+import DB from '@databases';
+import { HttpException } from '@exceptions/HttpException';
+import { User } from '@interfaces/users.interface';
+import { isEmpty } from '@utils/util';
+import { Model } from 'sequelize';
+
+/**
+ * Override: handle find record, check email, ...
+ * hash pwd, ...
+ * in route create, update
+ */
+class CRUDService<I, C extends Object, U extends Object> {
+  // extend Model
+  public table: any;
+  public tableName: string;
+
+  public constructor(tableName: string, sequenlizeDefiitionTable: any) {
+    this.tableName = tableName;
+    this.table = sequenlizeDefiitionTable;
+
+    if (!this.table) {
+      throw new Error('CRUDService: sequenlizeDefiitionTable cannot ' + this.table);
+    }
+  }
+
+  public async findAll(): Promise<I[]> {
+    const allRecord: I[] = await this.table.findAll();
+    return allRecord;
+  }
+
+  public async findById(id: number): Promise<I> {
+    if (isEmpty(id)) throw new HttpException(400, 'Id is empty');
+
+    const foundRecord: I = await this.table.findByPk(id);
+    if (!foundRecord) throw new HttpException(409, this.tableName + " doesn't exist");
+
+    return foundRecord;
+  }
+
+  public async create(data: C, callback?: (data: C) => Promise<C>): Promise<I> {
+    if (isEmpty(data)) throw new HttpException(400, this.tableName + ' data is empty');
+
+    if (callback) data = await callback(data);
+    console.log(data);
+
+    const createData: I = await this.table.create({ ...data });
+    return createData;
+  }
+
+  public async update(id: number, data: C, callback?: (data: C) => Promise<C>): Promise<I> {
+    if (isEmpty(data)) throw new HttpException(400, 'data is empty');
+
+    const findRecord: I = await this.table.findByPk(id);
+    if (!findRecord) throw new HttpException(409, this.tableName + " doesn't exist");
+
+    if (callback) data = await callback(data);
+
+    await this.table.update({ ...data }, { where: { id: id } });
+
+    const updated: I = await this.table.findByPk(id);
+    return updated;
+  }
+
+  public async delete(id: number): Promise<I> {
+    if (isEmpty(id)) throw new HttpException(400, "User doesn't existId");
+
+    const findRecord: I = await this.table.findByPk(id);
+    if (!findRecord) throw new HttpException(409, "User doesn't exist");
+
+    await this.table.destroy({ where: { id: id } });
+
+    return findRecord;
+  }
+}
+
+export default CRUDService;
